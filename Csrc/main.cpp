72c6de63ea462aea90d353e4542cfd4803c761ef
Til Goepfert <tilgoepfert@gmail.com>
2017-04-24 14:07:26 +0200

    added documentations

--- Csrc/main.cpp
+++ Csrc/main.cpp
@@ -3,24 +3,38 @@
 
 #define PI 3.141592653
 
+/**
+ * Approximates the value of sin for values in the interval [-PI/2,PI/2]
+ * @param x input value
+ * @return approximation of sin(x)
+ */
 double csin0(double x) {
   int32_t iter = 1;
   const int MAX = 42;
 
-  double result = x;
+  double valueI = x;
   double interResult = x;
 
+  /*
+   * loop calculates the iterative values based on the previous value, beginning with the second value with i = 1
+   * this limits the precision of the result to the limits of the double data format
+   */
   for (iter; iter < MAX; iter++) {
-    result = result * x * -x;
-    result = result / (2 * iter);
-    result = result / (2 * iter + 1);
+    valueI = valueI * x * -x;         // multiply the last x^(2i+1) by -x^2 (therefore includes also the factor -1^i)
+    valueI = valueI / (2 * iter);     // divide the result of valueI by 2i and (2* + 1) to avoid using the factorial
+    valueI = valueI / (2 * iter + 1); // of (2i + 1), that limit the number of executions
 
-    interResult += result;
+    interResult += valueI;
   }
 
   return interResult;
 }
 
+/**
+ * Approximates the sin of the input with the Taylor approximation
+ * @param x input value
+ * @return approximation of sin(x)
+ */
 double csin(double x) {
   if ((x <= PI / 2 && (x >= -PI / 2))) return csin0(x);
 
