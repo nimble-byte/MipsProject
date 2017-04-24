7ca2302246aea4d9fddd0735b8bde84f68a6a8ab
Til Goepfert <tilgoepfert@gmail.com>
2017-04-24 11:16:26 +0200

    optimized C++ code, Assembler implementation ready to be implemented

--- Csrc/main.cpp
+++ Csrc/main.cpp
@@ -3,57 +3,56 @@
 
 #define PI 3.141592653
 
-double pow(double base, int32_t exponent)
-{
-	if (exponent == 0) return 1;
-	return base * pow(base, exponent - 1);
+double pow(double base, int32_t exponent) {
+  if (exponent == 0) return 1;
+  return base * pow(base, exponent - 1);
 }
 
-int32_t factorial(int32_t number)
-{
-	if (number == 0) return 1;
-	return number * factorial(number - 1);
+int32_t factorial(int32_t number) {
+  if (number == 0) return 1;
+  return number * factorial(number - 1);
 }
 
-double csin0(double x)
-{
-	int32_t iter = 0;
-	const int MAX = 7;
+double csin0(double x) {
+  int32_t iter = 1;
+  const int MAX = 11;
 
-	double result = 0;
+  double result = x;
+  double interResult = x;
 
-	for (iter; iter < MAX; iter++) {
-		double fact = pow(-1, iter);
+  for (iter; iter < MAX; iter++) {
+    result = result * x * -x;
+    result = result / (2 * iter);
+    result = result / (2 * iter + 1);
 
-		double up = pow(x, (2 * iter + 1));
-		int32_t down = factorial(2 * iter + 1);
-		up = up / down;
+    interResult += result;
+  }
 
-		result += up * fact;
-	}
-
-	return result;
+  return interResult;
 }
 
-double csin(double x)
-{
-	double n = x;
-	while ((n > PI / 2) || (n < -PI / 2)) {
-		if (n > PI / 2) {
-			n -= PI;
-		}
-		else {
-			n += PI;
-		}
-	}
+double csin(double x) {
+  if ((x <= PI / 2 && (x >= -PI / 2))) return csin0(x);
 
-	std::cout << "Sin is calculated with the value " << n << std::endl;
-	return csin0(n);
+  double n = x;
+  while ((n > PI / 2) || (n < -PI / 2)) {
+    if (n > PI / 2) {
+      n -= PI;
+    } else {
+      n += PI;
+    }
+    n *= -1;
+  }
+
+  std::cout << "Sin is calculated with the value " << n << std::endl;
+  return csin0(n);
 }
 
-int main()
-{
-	std::cout << "The sin of " << PI << " is " << csin(PI / 2) << std::endl;
-	std::cout << "C++ libs say it is " << std::sin(PI / 2) << std::endl;
-	return 0;
+int main() {
+  std::cout << "The sin of " << 12 << " is " << csin(12) << std::endl;
+  std::cout << "C++ libs say it is " << std::sin(12) << std::endl << std::endl;
+
+  std::cout << "The sin of " << 3.14 << " is " << csin(3.14) << std::endl;
+  std::cout << "C++ libs say it is " << std::sin(3.14) << std::endl << std::endl;
+  return 0;
 }
